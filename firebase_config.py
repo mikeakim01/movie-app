7faca171a2ce0d77d86723f8e1a9f71bc5600809
spark_tech <96884221+mikeakim01@users.noreply.github.com>
2024-10-18 13:28:46 +0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -8,12 +8,13 @@ firebase_admin.initialize_app(cred)
 
 # Pyrebase configuration
 firebase_config = {
-    "apiKey": "a",
-    "authDomain": "b",
-    "projectId": "c",
-    "storageBucket": "d",
-    "messagingSenderId": "e",
-    "appId": "f"
+    "type": "service_account",
+    "apiKey": "AIzaSyCWqIBXEyFk9nHJc5UyEaCvHmgVrjKrzCE",
+    "authDomain": "pure-mission-337016.firebaseapp.com",
+    "projectId": "pure-mission-337016",
+    "storageBucket": "pure-mission-337016.appspot.com",
+    "messagingSenderId": "976188288424",
+    "appId": "1:976188288424:web:b64f0c6a1447958369c19d"
 }
 
 firebase = pyrebase.initialize_app(firebase_config)
